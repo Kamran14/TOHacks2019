2888c5364ae19950df24f46e06e2fa3f9e936f11
Jason Amri <jasonamri@gmail.com>
2019-06-22 22:48:06 -0400

    question gen

---
+++ VFquestiongenerator.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const extractor = require('./extractor');
+
+
+const questions = [
+    "hi1",
+    "hi2",
+    "hi3"
+]
+
+
+
+app.get('/', (req, res) => {
+    var question = questions[Math.floor(Math.random()*questions.length)];
+    res.send(question);
+});
+
+app.listen(port, () => console.log(`Question gen app listening on port ${port}!`));
